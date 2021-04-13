3f7630c1ef3f9f09c6d03c1b9b8495f0fed9a6fa
adel hosseiny <a.hosseiny@hamisystem.ir>
2021-04-13 15:59:54 +0430

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const axios = require("axios");
+const redis = require("redis");
+const app = express();
+
+const redisPort = 6379
+const client = redis.createClient(redisPort);
+
+client.on("error", (err) => {
+    console.log(err);
+})
+
+app.get("/jobs", (req, res) => {
+    const searchTerm = req.query.search;
+    try {
+        client.get(searchTerm, async (err, jobs) => {
+            if (err) throw err;
+    
+            if (jobs) {
+                res.status(200).send({
+                    message: "data retrieved from the cache",
+                    jobs: JSON.parse(jobs)
+                    
+                });
+            }
+            else {
+                const jobs = await axios.get(`https://jobs.github.com/positions.json?search=${searchTerm}`);
+                client.setex(searchTerm, 600, JSON.stringify(jobs.data));
+                res.status(200).send({
+                    message: "cache miss",
+                    jobs: jobs.data
+                    
+                });
+            }
+        });
+    } catch(err) {
+        res.status(500).send({message: err.message});
+    }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Node server started");
+});
